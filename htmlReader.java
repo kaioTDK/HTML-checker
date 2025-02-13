c8bd532136b2198f3bb1f4d0e203df0875c35314
kaio santos <kaiojugno@gmail.com>
2025-02-12 22:12:11 -0300

    added reader class

---
+++ htmlReader.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class htmlReader {
+    
+    public void htmlReader(BufferedReader inputBuff){
+        String input = "";
+        if ((input = inputBuff.readLine()) == null ){
+            return;
+        }
+    }
+}
