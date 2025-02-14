713f606e5d4aba4ba0cf097bf93ec089f9f4ecb2
kaio santos <kaiojugno@gmail.com>
2025-02-13 22:04:47 -0300

    working deepest text findercd

--- HtmlAnalyzer.java
+++ HtmlAnalyzer.java
@@ -5,15 +5,13 @@ public class HtmlAnalyzer {
  
      public static void main (String[] args) throws Exception{
             
-        String message = "";
-        int depth = 0;
-
-        URI uri = new URI(args[0]);
+        //URI uri = new URI(args[0]);
+        URI uri = new URI("http://hiring.axreng.com/internship/example1.html");
+        //URI uri = new URI("http://locahost:8000");
         URL url = uri.toURL();
         BufferedReader  inputBuffer = new BufferedReader(new InputStreamReader(url.openStream()));
-        String inputLine = "";
         
 
-        HtmlReader.htmlReader(inputBuffer);
+        HtmlReader.htmlReader(inputBuffer,0,0, "");
      }
 }
--- HtmlReader.java
+++ HtmlReader.java
@@ -1,13 +1,33 @@
 import java.io.*;
 
-public abstract class HtmlReader {
+public class HtmlReader {
     
-    public static void htmlReader(BufferedReader inputBuff) throws IOException{
-        String input = "";
-        if ((input = inputBuff.readLine()) == null ){
+    public static void htmlReader(BufferedReader inputBuff,int depth, int maxDepth, String message) throws IOException{
+        
+        String buffLine = "";
+
+        if ((buffLine = inputBuff.readLine()) == null ){
+            System.out.println(message);
+            System.out.println(maxDepth);
             return;
         }
-        System.out.println(input);
-        htmlReader(inputBuff);
+        System.out.println("i'm at line:" + buffLine);
+        
+        if (buffLine.trim().matches("<[a-z-A-Z-0-9- ]+>")){
+            htmlReader(inputBuff, depth + 1, maxDepth, message);
+
+
+        }
+        else if (buffLine.trim().matches("<[\\/][a-z-A-Z-0-9- ]+>") ){
+            htmlReader(inputBuff, depth - 1, maxDepth, message);
+        }
+        else if (!buffLine.matches("<([^<]+)>") & depth > maxDepth & !buffLine.isEmpty()){
+            message = buffLine.trim();
+            htmlReader(inputBuff, depth, depth, message);
+
+        }
+        else{
+            htmlReader(inputBuff, depth, maxDepth, message);
+        }
     }
 }
